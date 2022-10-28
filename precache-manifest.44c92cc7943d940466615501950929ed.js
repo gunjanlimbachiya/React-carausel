a725e68dd899e1ea9676022a9aa48da84491fc33
Gunjan <gunjan.limbachiya@gmail.com>
2022-10-28 14:53:38 +0530

    Updates

---
+++ precache-manifest.44c92cc7943d940466615501950929ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56d43d78d4de634a77ea2e726bd5c29",
+    "url": "/React-carausel/index.html"
+  },
+  {
+    "revision": "159013865d37a0a33614",
+    "url": "/React-carausel/static/css/main.39689632.chunk.css"
+  },
+  {
+    "revision": "ffce46ce27e8a35c0b93",
+    "url": "/React-carausel/static/js/2.5a7bb05f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-carausel/static/js/2.5a7bb05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159013865d37a0a33614",
+    "url": "/React-carausel/static/js/main.618b5a47.chunk.js"
+  },
+  {
+    "revision": "2a9592b70beadbb2ab4a",
+    "url": "/React-carausel/static/js/runtime-main.09f696e5.js"
+  }
+]);
